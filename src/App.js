4f509466bbb2b159da9ef1bee6d71913402293cf
Silverio Rodriguez <silverio.rdza@gmail.com>
2021-11-13 17:52:36 -0500

    Initial setup of the Materialize library and css also the Material ICONS link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React, {useEffect} from 'react'
+
+//Import the css from materialize, and also the M function of it.
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 import './App.css';
 
 const App = () => {
+
+  useEffect(()=>{
+    // Initialize Materialize JS for a better syling of the webpage
+    M.AutoInit(); //Now we will be able to use modal dialogs, buttons and other stuff.
+  });
+
   return (
     <div className="App">My App</div>
   );
