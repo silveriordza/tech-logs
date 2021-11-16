f13006af89caab99da045bbf1904e7d8cdd5dbd6
Silverio Rodriguez <silverio.rdza@gmail.com>
2021-11-15 20:19:23 -0500

    Final Application functionality added to delete a Technician. This is the final commit, the app is ready and done

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -4,7 +4,7 @@ import { GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECHS_ERROR } from "./ty
 export const getTechs = () => async dispatch => {
 	
 	try {
-		setLoading(true);
+		setLoading();
 		const res = await fetch('/techs');
 		const data = await res.json();
 		dispatch({
@@ -37,6 +37,27 @@ export const addTech = tech => async dispatch => {
 		dispatch({
 			type: ADD_TECH,
 			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: err.response.statusText
+		})
+	}
+};
+
+// Delete techs from server
+export const deleteTech = id => async dispatch => {
+	
+	try {
+		setLoading();
+		await fetch(`/techs/${id}`, {
+			method: 'DELETE'
+		});
+
+		dispatch({
+			type: DELETE_TECH,
+			payload: id
 		});
 	} catch (err) {
 		dispatch({
